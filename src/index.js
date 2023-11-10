12b2ae93290b9adfb6a97f0550b983b4b84ebd4f
Diamond Haley <129228600+developingdiamond@users.noreply.github.com>
2023-11-10 13:46:45 -0600

    Update index.js
    cursor animation add

--- src/index.js
+++ src/index.js
@@ -39,6 +39,18 @@ function stop (){
   stopAudio(song)
 }
 
+ function cursorAnimation() {
+   let body = document.getElementsByTagName("body")[0];
+
+  body.style.cursor= "url('https://github.com/gabrielsanchez/erddiagram/blob/main/assets/mallet.png'), auto";
+ 
+  setTimeout(() => {
+   body.style.cursor = "url('https://github.com/gabrielsanchez/erddiagram/blob/main/assets/mallet.png'), auto";
+ 
+  }, 300);
+ };
+
+
  
 /**
  * Generates a random integer within a range.
@@ -254,6 +266,7 @@ function startTimer() {
 */
 function whack(event) {
   // TODO: Write your code here.
+  cursorAnimation();
   console.log("whack!")
   updateScore();
   playAudio(audioHit);
